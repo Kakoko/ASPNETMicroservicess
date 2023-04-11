aa3fb327866d9b3d3896428811786b475dd2f325
Simuzeche Kakoko Kaluwa <simuzeche.kaluwa@angledimension.com>
2023-04-11 10:41:57 +0200

    Feat: Added Repositories to the DI Container

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,11 +1,19 @@
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<ICatalogContext , CatalogContext>();
+builder.Services.AddScoped<IProductRepository , ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
