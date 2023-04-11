144ef69bc90dc692f43f260cc9fbd8a786f8f890
Simuzeche Kakoko Kaluwa <simuzeche.kaluwa@angledimension.com>
2023-04-11 10:59:02 +0200

    Fead: Added Docker Container Orchastration Support

--- src/Services/Catalog/Catalog.API/Entities/Product.cs
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -10,11 +10,11 @@ namespace Catalog.API.Entities
         public string Id { get; set; }
 
         [BsonElement("Name")]
-        public required string Name { get; set; }
-        public required string Category { get; set; }
-        public required string Summary { get; set; }
-        public required string Description { get; set; }
-        public required string ImageFile { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
         public decimal Price { get; set; }
     }
 }
